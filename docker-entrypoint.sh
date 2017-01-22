4938ed406c277b7d1647ea83589cec9534e6cb0f
rob <rob@unx.pl>
2017-01-22 02:07:56 +0100

    set timezone quietly

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ -n "$TZ" ] && [ -e /usr/share/zoneinfo/$TZ ]; then
 	ln -fs /usr/share/zoneinfo/$TZ /etc/localtime
-	dpkg-reconfigure -f noninteractive tzdata
+	dpkg-reconfigure -f noninteractive tzdata 2>/dev/null
 fi
 
 confd -onetime -backend env
